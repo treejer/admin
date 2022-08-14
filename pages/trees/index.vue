6dffbefb8843665bc41f82cd3d08028a08c36f52
iraj habibzadeh <iraj.habibzadeh70@gmail.com>
2022-08-14 13:52:29 +0430

    fix admin trees page ui issues

--- pages/trees/index.vue
+++ pages/trees/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container trees-admin">
     <div class="row pl-3 pr-3" v-if="items">
-      <div class="users over-flow-scroll col-12 col-xl-12 col-lg-11 offset-lg-1 offset-xl-1">
+      <div
+        class="users over-flow-scroll col-12 col-xl-12 col-lg-11 offset-lg-1 offset-xl-1"
+      >
         <div class="header-tab position-relative pb-3">
           <div class="row">
             <div class="col-md-12 p-md-0">
@@ -13,46 +15,66 @@
         <div class="row">
           <div class="col-12 mb-3 pl-0">
             <ul class="">
-              <li class="d-inline-block list-style-none pr-2 pb-2" v-for="(item, index) in tabs" :key="index">
-                <button @click="setIndex(item.text, index)" :class="tabsIndex === index ? 'btn-green' : 'btn-gray'"
-                  :key="index">
+              <li
+                class="d-inline-block list-style-none pr-2 pb-2"
+                v-for="(item, index) in tabs"
+                :key="index"
+              >
+                <button
+                  @click="setIndex(item.text, index)"
+                  :class="tabsIndex === index ? 'btn-green' : 'btn-gray'"
+                  :key="index"
+                >
                   {{ item.text }}
                 </button>
               </li>
             </ul>
           </div>
           <div v-show="tabsIndex === 0" class="row">
-            <div class="col-12  ">
-              <div class="position-relative w-50 search-admin-user-box">
-                <!-- <input class="search-admin-user" v-model="searchAdminUsers"
+            <div class="position-relative col-md-6 search-admin-user-box">
+              <!-- <input class="search-admin-user" v-model="searchAdminUsers"
                   placeholder="Search by Id or planter address" @keyup.enter="searchAdminUsers" />
                 <img src="~/assets/images/tree-profile/search.svg" alt="search" class="search-icon" /> -->
 
-                 <input class="search-admin-user" v-model="query.planter"
-                  placeholder="Planter Adress" @change="filterTrees()" />
-                <img src="~/assets/images/tree-profile/search.svg" alt="search" class="search-icon" />
-
-                <p>
-                  Have Update
-
-                  <select v-model="query.haveUpdate" @change="filterTrees()">
-                    <option value="0">All</option>
-                    <option value="1">Yes</option>
-                    <option value="2">No</option>
-                  </select>
-                </p>
-
-
-               
-
-                
-
+              <input
+                class="search-admin-user"
+                v-model="query.planter"
+                placeholder="Planter Adress"
+                @change="filterTrees()"
+              />
+              <img
+                src="~/assets/images/tree-profile/search.svg"
+                alt="search"
+                class="search-icon"
+              />
+            </div>
+            <div class="col-md-6 justify-content-end">
+              <div class="param tr-gray-three w-100 text-right pr-md-5 ">
+                <span class="font-weight-bolder">Have Update</span>
+                <select
+                  class="bg-gray select-box-have-update tr-gray-four"
+                  v-model="query.haveUpdate"
+                  @change="filterTrees()"
+                >
+                  <option value="0">All</option>
+                  <option value="1">Yes</option>
+                  <option value="2">No</option>
+                </select>
               </div>
             </div>
             <div class="col-12 col-md-12 p-0 p-md-3">
               <div class="admin-user-table">
-                <b-table striped :current-page="currentPage" :per-page="perPage" :items="items"
-                  class="param tr-gray-three" hover :filter="searchAdminUsers" :fields="fields" id="tree-table">
+                <b-table
+                  striped
+                  :current-page="currentPage"
+                  :per-page="perPage"
+                  :items="items"
+                  class="param tr-gray-three"
+                  hover
+                  :filter="searchAdminUsers"
+                  :fields="fields"
+                  id="tree-table"
+                >
                   <template #cell(Planter)="data">
                     <span>{{ data.value }}</span>
                   </template>
@@ -63,38 +85,88 @@
                   </template>
                 </b-table>
               </div>
-              <b-pagination class="mt-4" v-model="currentPage" :total-rows="skip" :per-page="perPage" align="fill"
-                size="sm">
+              <b-pagination
+                class="mt-4"
+                v-model="currentPage"
+                :total-rows="skip"
+                :per-page="perPage"
+                align="fill"
+                size="sm"
+              >
               </b-pagination>
-              <div v-if="skip <= 20" class="d-flex justify-content-center position-relative arrows-box">
-                <button :style="`width : 175px;left:19.9%`" class="pointer-event" @click.prevent="prePage()">
+              <div
+                v-if="skip <= 20"
+                class="d-flex justify-content-center position-relative arrows-box"
+              >
+                <button
+                  :style="`width : 175px;left:19.9%`"
+                  class="pointer-event"
+                  @click.prevent="prePage()"
+                >
                   Pre
                 </button>
-                <button :style="`width : 175px;left:66.8%`" class="pointer-event" @click.prevent="nextPage()">
+                <button
+                  :style="`width : 175px;left:66.8%`"
+                  class="pointer-event"
+                  @click.prevent="nextPage()"
+                >
                   Next
                 </button>
               </div>
-              <div v-else-if="skip <= 60" class="d-flex justify-content-center position-relative arrows-box">
-                <button :style="`width :200px;left: 10%`" class="pointer-event" @click.prevent="prePage()">
+              <div
+                v-else-if="skip <= 60"
+                class="d-flex justify-content-center position-relative arrows-box"
+              >
+                <button
+                  :style="`width :200px;left: 10%`"
+                  class="pointer-event"
+                  @click.prevent="prePage()"
+                >
                   Pre
                 </button>
-                <button :style="`width : 200px;left: 66%`" class="pointer-event" @click.prevent="nextPage()">
+                <button
+                  :style="`width : 200px;left: 66%`"
+                  class="pointer-event"
+                  @click.prevent="nextPage()"
+                >
                   Next
                 </button>
               </div>
-              <div v-else-if="skip >= 60" class="d-flex justify-content-center position-relative arrows-box">
-                <button :style="`width :114px;left: 11.2%`" class="pointer-event" @click.prevent="prePage()">
+              <div
+                v-else-if="skip >= 60"
+                class="d-flex justify-content-center position-relative arrows-box"
+              >
+                <button
+                  :style="`width :114px;left: 11.2%`"
+                  class="pointer-event"
+                  @click.prevent="prePage()"
+                >
                   Pre
                 </button>
-                <button :style="`width : 114px;left: 78.2%`" class="pointer-event" @click.prevent="nextPage()">
+                <button
+                  :style="`width : 114px;left: 78.2%`"
+                  class="pointer-event"
+                  @click.prevent="nextPage()"
+                >
                   Next
                 </button>
               </div>
-              <div v-else class="d-flex justify-content-center position-relative arrows-box">
-                <button :style="`width :175px;left:10%`" class="pointer-event" @click.prevent="prePage()">
+              <div
+                v-else
+                class="d-flex justify-content-center position-relative arrows-box"
+              >
+                <button
+                  :style="`width :175px;left:10%`"
+                  class="pointer-event"
+                  @click.prevent="prePage()"
+                >
                   Pre
                 </button>
-                <button :style="`width :175px;left:75%`" class="pointer-event" @click.prevent="nextPage()">
+                <button
+                  :style="`width :175px;left:75%`"
+                  class="pointer-event"
+                  @click.prevent="nextPage()"
+                >
                   Next
                 </button>
               </div>
@@ -202,24 +274,26 @@ export default {
       self.loading = true;
 
       let whereQuery = "";
-      if(this.query.haveUpdate == 1){
-        whereQuery+=`lastUpdate_not: null`;
+      if (this.query.haveUpdate == 1) {
+        whereQuery += `lastUpdate_not: null`;
       }
 
-      if(this.query.planter){
-        whereQuery+=`, planter: "${this.query.planter.toLowerCase()}"`;
+      if (this.query.planter) {
+        whereQuery += `, planter: "${this.query.planter.toLowerCase()}"`;
       }
 
-      
-      if(whereQuery != "") {
-        whereQuery = `{${whereQuery}}`
+      if (whereQuery != "") {
+        whereQuery = `{${whereQuery}}`;
       }
 
       await self.$axios
         .$post(`${process.env.GRAPHQL_URL}`, {
           query: `{
-             trees(first:${self.currentPage * self.perPage},skip:${self.skip
-            },orderBy: createdAt, orderDirection: desc ${whereQuery ? `,where: ${whereQuery}` : ""}) {
+             trees(first:${self.currentPage * self.perPage},skip:${
+            self.skip
+          },orderBy: createdAt, orderDirection: desc ${
+            whereQuery ? `,where: ${whereQuery}` : ""
+          }) {
                   id
 	              	planter {id}
 	              	plantDate
@@ -260,19 +334,21 @@ export default {
                 Planter: item.planter ? item.planter.id : null,
                 TreeSpecsEntity:
                   item.treeSpecsEntity &&
-                    item.treeSpecsEntity.latitude &&
-                    item.treeSpecsEntity.longitude
+                  item.treeSpecsEntity.latitude &&
+                  item.treeSpecsEntity.longitude
                     ? item.treeSpecsEntity.latitude +
-                    "," +
-                    item.treeSpecsEntity.longitude
+                      "," +
+                      item.treeSpecsEntity.longitude
                     : "Empty",
-                LastUpdateAt: item.lastUpdate ? self
-                  .$moment(item.lastUpdate.createdAt * 1000)
-                  .strftime("%Y-%m-%d %I:%M:%S"): '-',
+                LastUpdateAt: item.lastUpdate
+                  ? self
+                      .$moment(item.lastUpdate.createdAt * 1000)
+                      .strftime("%Y-%m-%d %I:%M:%S")
+                  : "-",
                 showDetail: item.id,
               });
             });
-            
+
             console.log(self.items, "self.items is here");
             this.$forceUpdate();
           }
@@ -356,6 +432,14 @@ export default {
     border-radius: 18px;
     padding: 5px 15px;
     font-size: 14px;
+  }
+  .select-box-have-update {
+    background: #e5e7db;
+    border: 2px solid #fff;
+    border-radius: 6px;
+    padding: 5px 10px;
+    option {
+    }
   }
 
   .search-icon {
